ea7ef8086843d01286c18d51ed8821e05735218f
LuisVynicius <luismevy@gmail.com>
2026-01-20 23:26:31 -0300

    ADD: Profile Page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginPage } from './pages/login-page/login-page';
 import { StockPage } from './pages/stock-page/stock-page';
 import { AdminPage } from './pages/admin-page/admin-page';
+import { ProfilePage } from './pages/profile-page/profile-page';
 
 export const routes: Routes = [
     {
@@ -18,5 +19,10 @@ export const routes: Routes = [
         path: "admin",
         pathMatch: "full",
         component: AdminPage
+    },
+    {
+        path: "profile",
+        pathMatch: "full",
+        component: ProfilePage
     },
 ];
--- src/app/pages/profile-page/profile-page.ts
+++ src/app/pages/profile-page/profile-page.ts
@@ -3,14 +3,14 @@ import { FormInput } from '../shared/form-input/form-input';
 import { Button } from '../shared/button/button';
 
 @Component({
-  selector: 'app-login-page',
+  selector: 'app-profile-page',
   imports: [
     FormInput,
     Button
   ],
-  templateUrl: './login-page.html',
-  styleUrl: './login-page.css',
+  templateUrl: './profile-page.html',
+  styleUrl: './profile-page.css',
 })
-export class LoginPage {
+export class ProfilePage {
 
 }
